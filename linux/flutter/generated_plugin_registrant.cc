4d9f573ab0b4b391c7166521eb1817be0a2b9b5f
dreamAviator <nathan@baron.berlin>
2026-01-29 22:48:27 +0100

    android basically on par with pc now doing metadata stuff stuck on trying to make a file out of a uri or at least get the file path

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <audiotags/audiotags_plugin.h>
 #include <dynamic_color/dynamic_color_plugin.h>
 #include <media_kit_libs_linux/media_kit_libs_linux_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) audiotags_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "AudiotagsPlugin");
+  audiotags_plugin_register_with_registrar(audiotags_registrar);
   g_autoptr(FlPluginRegistrar) dynamic_color_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "DynamicColorPlugin");
   dynamic_color_plugin_register_with_registrar(dynamic_color_registrar);
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <audiotags/audiotags_plugin_c_api.h>
 #include <dynamic_color/dynamic_color_plugin_c_api.h>
 #include <media_kit_libs_windows_audio/media_kit_libs_windows_audio_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudiotagsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudiotagsPluginCApi"));
   DynamicColorPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DynamicColorPluginCApi"));
   MediaKitLibsWindowsAudioPluginCApiRegisterWithRegistrar(
