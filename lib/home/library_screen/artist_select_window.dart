9e56f03134926a2d24606c4b48fab01e9376f6be
dreamAviator <nathan@baron.berlin>
2026-07-22 01:55:17 +0200

    continued rework of player skips to next item after the current one is finished now trying to fix play/pause button tried to implement scrobble trying to implement marquee text next finished song tab (as far as the other tabs) continued artist select

--- lib/home/library_screen/artist_select_window.dart
+++ lib/home/library_screen/artist_select_window.dart
@@ -5,11 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:image_card/image_card.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
+import '../../main.dart';
 import 'artist_screen/artist_screen.dart';
 
 class ArtistSelectWindow {//TODO:joa das hier machen nh :P
-  static void showArtistSelectWindow(BuildContext context,List<dynamic> artistIDs) {
+  static void showArtistSelectWindow(BuildContext context,List<dynamic> artistInfosLite) {
+    final riverpodManager = RiverpodManager();
     Size screenSize = MediaQuery.of(context).size;
     showDialog(
       context: context,
@@ -17,24 +20,52 @@ class ArtistSelectWindow {//TODO:joa das hier machen nh :P
       builder: (BuildContext context) {
         return StatefulBuilder(
           builder: (context,setState) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: artistIDs.length,
-                  itemBuilder: (BuildContext context, index) {
-                    return Card(
-                      child: ListTile(
-                        title: Text(artistIDs[index]['name']),
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => ArtistScreen(artistID: artistIDs[index]['id'])));
-                        },
+            return AlertDialog(
+              title: Text("Select artist"),
+              content: MasonryGridView.count(
+                crossAxisCount: (screenSize.width / 200).toInt(),
+                itemCount: artistInfosLite.length,
+                itemBuilder: (context,index) {
+                  return Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        debugPrint('Card tapped.');
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => ArtistScreen(artistID: artistInfosLite[index]['id'])));
+                      },
+                      child: Column(
+                        children: [
+                          Consumer(
+                            builder: (context, ref, child) {
+                              final artistDetails = ref.watch(riverpodManager.artistDetailsProvider(artistInfosLite[index]['id']));
+                              return AspectRatio(
+                                aspectRatio: 1,
+                                child: switch (artistDetails) {
+                                  AsyncValue(:final value?) => CachedNetworkImage(
+                                    imageUrl: "${subsonicService.getURL(null, null, null)[0]}getCoverArt${subsonicService.getURL(null, null, null)[1]}&id=${value['coverArt']}",
+                                    progressIndicatorBuilder: (context, url, downloadProgress) =>
+                                        LoadingAnimationWidget.fourRotatingDots(color: Colors.purple, size: 25),
+                                    errorWidget: (context, url, error) => IconButton(
+                                      onPressed: () {
+                                        //hier retry
+                                      },
+                                      icon: Icon(Icons.error),
+                                    ),
+                                  ),
+                                  AsyncValue(error: != null) => const Text("error"),
+                                  AsyncValue() => LoadingAnimationWidget.fourRotatingDots(color: Colors.purple, size: 25),
+                                }
+                              );
+                            },
+                          ),
+                          Text(artistInfosLite[index]['name']),
+                        ],
                       ),
-                    );
-                  },
-                ),
-              ],
+                    ),
+                  );
+                },
+              )
             );
           },
         );
