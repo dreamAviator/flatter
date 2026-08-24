b4358281433c1880f1853ca50f7bfe4abc76b102
dreamAviator <nathan@baron.berlin>
2026-08-24 17:59:58 +0200

    continued rework of player skips to next item after the current one is finished now fixed the play/pause button in app ui, the os ui is broken now tried to implement scrobble trying to implement marquee text next finished song tab (as far as the other tabs) finished artist select last library tab now gets saved the song tile slidable action for go to artist now opens the artist select popup added playlist comment on mobile layout works on android again added marquee effect in playlist_screen.dart title and made comment scrollable on mobile, also configured the edit_playlist_popup to disable the comment and public setting when creating a playlist, i plan to make these settings available later but right now it's a bit hard finished to add favorite/unfavorite to itemMenus.dart started the ability to favorite playlists in navidrome mode /even tho server does not support that) organized database code and files fixed a bug i found a fork that fixes the issue with the masonry grid

--- README.md
+++ README.md
@@ -18,3 +18,8 @@ A cross platform Navidrome client
 - automatically fetch Lyrics from the web
 - play local music
   - batch metadata editor built in
+
+
+## other stuff
+### Notice regarding the flutter_staggered_gridview_package
+In this app, the [flutter_staggered_gridview](https://pub.dev/packages/flutter_staggered_grid_view) package is used. It contains a bug however, that was fixed in a different [branch](https://github.com/letsar/flutter_staggered_grid_view/tree/feature/masonry_cache?tab=readme-ov-file). Because this branch is not yet published on pub.dev, the whole branch is included in this project and repository (using a github url did not work for me).
---
+++ flutter_staggered_grid_view-feature-masonry_cache/examples/lib/main_masonry_variable_size.dart
@@ -0,0 +1,59 @@
+import 'package:examples/common.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Staggered Grid View Demo',
+      showPerformanceOverlay: true,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverMasonryGrid.count(
+            crossAxisCount: 4,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            childCount: 40,
+            itemBuilder: (context, index) {
+              if (index == 1) {
+                return const AnimatedTile(
+                  index: 1,
+                  minExtent: 100,
+                  maxExtent: 400,
+                  backgroundColor: Colors.red,
+                );
+              }
+              return Tile(
+                index: index,
+                extent: (index % 5 + 1) * 100,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
