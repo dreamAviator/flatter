53b503333b9bbb280233bc42fa7caf37e51141c9
dreamAviator <nathan@baron.berlin>
2026-09-07 20:44:55 +0200

    if an action is added or removed to/from the menu actions, it gets added/removed

--- lib/settings/settings_controller.dart
+++ lib/settings/settings_controller.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:toml/toml.dart';
 
@@ -98,6 +97,7 @@ class SettingsController {
     for (String key in keysToRemove) {
       settingsMap.remove(key);
     }
+    _checkAndCorrectMenuActions();
     if (settingsMap['firstStart'] == true) {
       firstStart();
     }
@@ -133,5 +133,261 @@ class SettingsController {
     path = "${path}/flatter_settings.toml";
     TomlDocument settingsDocument = TomlDocument.fromMap(settingsMap);
     await settingsDocument.save(path);
+  }
+
+  //additional functions
+  void _checkAndCorrectMenuActions() {
+    //songMenuActionOrder check
+    List<dynamic> songMenuActions = [];
+    songMenuActions = songMenuActions + settingsMap['songMenuActionOrder']['mainMenu'];
+    songMenuActions = songMenuActions + settingsMap['songMenuActionOrder']['moreSheet'];
+    songMenuActions = songMenuActions + settingsMap['songMenuActionOrder']['unused'];
+    for (String action in defaultSettingsMap['songMenuActionOrder']['mainMenu']) {
+      if (songMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['songMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['songMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['songMenuActionOrder']['moreSheet']) {
+      if (songMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['songMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['songMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['songMenuActionOrder']['unused']) {
+      if (songMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['songMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['songMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    songMenuActions.clear();
+    songMenuActions = songMenuActions + defaultSettingsMap['songMenuActionOrder']['mainMenu'];
+    songMenuActions = songMenuActions + defaultSettingsMap['songMenuActionOrder']['moreSheet'];
+    songMenuActions = songMenuActions + defaultSettingsMap['songMenuActionOrder']['unused'];
+    List<dynamic> actionsToRemove = [];
+    for (String action in settingsMap['songMenuActionOrder']['mainMenu']) {
+      if (songMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    List<dynamic> mainMenuActions = settingsMap['songMenuActionOrder']['mainMenu'];
+    for (String action in actionsToRemove) {
+      mainMenuActions.remove(action);
+    }
+    settingsMap['songMenuActionOrder']['mainMenu'] = mainMenuActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['songMenuActionOrder']['moreSheet']) {
+      if (songMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    List<dynamic> moreSheetActions = settingsMap['songMenuActionOrder']['moreSheet'];
+    for (String action in actionsToRemove) {
+      moreSheetActions.remove(action);
+    }
+    settingsMap['songMenuActionOrder']['moreSheet'] = moreSheetActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['songMenuActionOrder']['unused']) {
+      if (songMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    List<dynamic> unusedActions = settingsMap['songMenuActionOrder']['unused'];
+    for (String action in actionsToRemove) {
+      unusedActions.remove(action);
+    }
+    settingsMap['songMenuActionOrder']['unused'] = unusedActions;
+    actionsToRemove.clear();
+    //albumMenuActionOrder check
+    List<dynamic> albumMenuActions = [];
+    albumMenuActions = albumMenuActions + settingsMap['albumMenuActionOrder']['mainMenu'];
+    albumMenuActions = albumMenuActions + settingsMap['albumMenuActionOrder']['moreSheet'];
+    albumMenuActions = albumMenuActions + settingsMap['albumMenuActionOrder']['unused'];
+    for (String action in defaultSettingsMap['albumMenuActionOrder']['mainMenu']) {
+      if (albumMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['albumMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['albumMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['albumMenuActionOrder']['moreSheet']) {
+      if (albumMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['albumMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['albumMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['albumMenuActionOrder']['unused']) {
+      if (albumMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['albumMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['albumMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    albumMenuActions.clear();
+    albumMenuActions = albumMenuActions + defaultSettingsMap['albumMenuActionOrder']['mainMenu'];
+    albumMenuActions = albumMenuActions + defaultSettingsMap['albumMenuActionOrder']['moreSheet'];
+    albumMenuActions = albumMenuActions + defaultSettingsMap['albumMenuActionOrder']['unused'];
+    for (String action in settingsMap['albumMenuActionOrder']['mainMenu']) {
+      if (albumMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    mainMenuActions = settingsMap['albumMenuActionOrder']['mainMenu'];
+    for (String action in actionsToRemove) {
+      mainMenuActions.remove(action);
+    }
+    settingsMap['albumMenuActionOrder']['mainMenu'] = mainMenuActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['albumMenuActionOrder']['moreSheet']) {
+      if (albumMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    moreSheetActions = settingsMap['albumMenuActionOrder']['moreSheet'];
+    for (String action in actionsToRemove) {
+      moreSheetActions.remove(action);
+    }
+    settingsMap['albumMenuActionOrder']['moreSheet'] = moreSheetActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['albumMenuActionOrder']['unused']) {
+      if (albumMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    unusedActions = settingsMap['albumMenuActionOrder']['unused'];
+    for (String action in actionsToRemove) {
+      unusedActions.remove(action);
+    }
+    settingsMap['albumMenuActionOrder']['unused'] = unusedActions;
+    actionsToRemove.clear();
+    //artistMenuActionOrder check
+    List<dynamic> artistMenuActions = [];
+    artistMenuActions = artistMenuActions + settingsMap['artistMenuActionOrder']['mainMenu'];
+    artistMenuActions = artistMenuActions + settingsMap['artistMenuActionOrder']['moreSheet'];
+    artistMenuActions = artistMenuActions + settingsMap['artistMenuActionOrder']['unused'];
+    for (String action in defaultSettingsMap['artistMenuActionOrder']['mainMenu']) {
+      if (artistMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['artistMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['artistMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['artistMenuActionOrder']['moreSheet']) {
+      if (artistMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['artistMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['artistMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['artistMenuActionOrder']['unused']) {
+      if (artistMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['artistMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['artistMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    artistMenuActions.clear();
+    artistMenuActions = artistMenuActions + defaultSettingsMap['artistMenuActionOrder']['mainMenu'];
+    artistMenuActions = artistMenuActions + defaultSettingsMap['artistMenuActionOrder']['moreSheet'];
+    artistMenuActions = artistMenuActions + defaultSettingsMap['artistMenuActionOrder']['unused'];
+    for (String action in settingsMap['artistMenuActionOrder']['mainMenu']) {
+      if (artistMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    mainMenuActions = settingsMap['artistMenuActionOrder']['mainMenu'];
+    for (String action in actionsToRemove) {
+      mainMenuActions.remove(action);
+    }
+    settingsMap['artistMenuActionOrder']['mainMenu'] = mainMenuActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['artistMenuActionOrder']['moreSheet']) {
+      if (artistMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    moreSheetActions = settingsMap['artistMenuActionOrder']['moreSheet'];
+    for (String action in actionsToRemove) {
+      moreSheetActions.remove(action);
+    }
+    settingsMap['artistMenuActionOrder']['moreSheet'] = moreSheetActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['artistMenuActionOrder']['unused']) {
+      if (artistMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    unusedActions = settingsMap['artistMenuActionOrder']['unused'];
+    for (String action in actionsToRemove) {
+      unusedActions.remove(action);
+    }
+    settingsMap['artistMenuActionOrder']['unused'] = unusedActions;
+    actionsToRemove.clear();
+    //playlistMenuActionOrder check
+    List<dynamic> playlistMenuActions = [];
+    playlistMenuActions = playlistMenuActions + settingsMap['playlistMenuActionOrder']['mainMenu'];
+    playlistMenuActions = playlistMenuActions + settingsMap['playlistMenuActionOrder']['moreSheet'];
+    playlistMenuActions = playlistMenuActions + settingsMap['playlistMenuActionOrder']['unused'];
+    for (String action in defaultSettingsMap['playlistMenuActionOrder']['mainMenu']) {
+      if (playlistMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['playlistMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['playlistMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['playlistMenuActionOrder']['moreSheet']) {
+      if (playlistMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['playlistMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['playlistMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    for (String action in defaultSettingsMap['playlistMenuActionOrder']['unused']) {
+      if (playlistMenuActions.contains(action) == false) {
+        List<dynamic> moreSheetActions = settingsMap['playlistMenuActionOrder']['moreSheet'];
+        moreSheetActions.add(action);
+        settingsMap['playlistMenuActionOrder']['moreSheet'] = moreSheetActions;
+      }
+    }
+    playlistMenuActions.clear();
+    playlistMenuActions = playlistMenuActions + defaultSettingsMap['playlistMenuActionOrder']['mainMenu'];
+    playlistMenuActions = playlistMenuActions + defaultSettingsMap['playlistMenuActionOrder']['moreSheet'];
+    playlistMenuActions = playlistMenuActions + defaultSettingsMap['playlistMenuActionOrder']['unused'];
+    for (String action in settingsMap['playlistMenuActionOrder']['mainMenu']) {
+      if (playlistMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    mainMenuActions = settingsMap['playlistMenuActionOrder']['mainMenu'];
+    for (String action in actionsToRemove) {
+      mainMenuActions.remove(action);
+    }
+    settingsMap['playlistMenuActionOrder']['mainMenu'] = mainMenuActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['playlistMenuActionOrder']['moreSheet']) {
+      if (playlistMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    moreSheetActions = settingsMap['playlistMenuActionOrder']['moreSheet'];
+    for (String action in actionsToRemove) {
+      moreSheetActions.remove(action);
+    }
+    settingsMap['playlistMenuActionOrder']['moreSheet'] = moreSheetActions;
+    actionsToRemove.clear();
+    for (String action in settingsMap['playlistMenuActionOrder']['unused']) {
+      if (playlistMenuActions.contains(action) == false) {
+        actionsToRemove.add(action);
+      }
+    }
+    unusedActions = settingsMap['playlistMenuActionOrder']['unused'];
+    for (String action in actionsToRemove) {
+      unusedActions.remove(action);
+    }
+    settingsMap['playlistMenuActionOrder']['unused'] = unusedActions;
   }
 }
