9bc67d8355349f7c2d29cd304bcaca1db89a8de7
dreamaviator <130237036+dreamAviator@users.noreply.github.com>
2026-09-10 18:20:00 +0200

    continued implementing persistent local queue

--- lib/storage/local_not_database_storage_controller.dart
+++ lib/storage/local_not_database_storage_controller.dart
@@ -1,24 +1,38 @@
+import 'dart:io';
+
 import 'package:audio_service/audio_service.dart';
 import 'package:flatter/main.dart';
 import 'package:flatter/useful_scripts.dart';
 import 'package:toml/toml.dart';
 
 class LocalNotDatabaseStorageController {
-  final usefulScripts = OtherScripts();
-  void saveQueue() async {//TODO:vlt eine möglichkeit finden, das als mediaitem list zu speichern. wäre einfacher.
-    List<String> idList = usefulScripts.queueToIDlist(await playerControl.customAction('getQueue'));
+  final usefulScripts = SubsonicJustAudioCompatibility();
+  void saveQueue() async {
+    List<MediaItem> queue = await playerControl.customAction('getQueue');
+    List<dynamic> queueSubsonicSongs = usefulScripts.mediaItemListToSubsonicSongList(queue);
     String dataDirectory = pathProvider.dataDirectory;
     String path = "$dataDirectory/flatter_persistent_queue.toml";
     int position = await playerControl.customAction('getCurrentIndex');
     Map<dynamic,dynamic> persistentQueueMap = {
-      'queue':idList,
-      'position':position
+      'queue':queueSubsonicSongs,
+      'position':position,//not needed, because essentially the mediaitems with the "current" attribute are saved
     };
     TomlDocument persistentQueueDocument = TomlDocument.fromMap(persistentQueueMap);
     persistentQueueDocument.save(path);
   }
 
   Future<List<MediaItem>> loadQueue() async {
-    return [];
+    TomlDocument persistentQueueDocument;
+    String dataDirectory = pathProvider.dataDirectory;
+    String path = "$dataDirectory/flatter_persistent_queue.toml";
+    if (await File(path).exists() == false) {
+      return [];
+    }
+    persistentQueueDocument = await TomlDocument.load(path);
+    Map<dynamic,dynamic> persistentQueueMap = persistentQueueDocument.toMap();
+    int position = persistentQueueMap['position'];//not needed, because essentially the mediaitems with the "current" attribute are saved
+    List<dynamic> queueSubsonicSongs = persistentQueueMap['queue'];
+    List<MediaItem> queue = usefulScripts.subsonicSongListToMediaItemList(queueSubsonicSongs);
+    return queue;
   }
 }
--- lib/useful_scripts.dart
+++ lib/useful_scripts.dart
@@ -30,6 +30,28 @@ class SubsonicJustAudioCompatibility {
     });
     return MediaItem(id: id, title: title, album: album, artist: artist, duration: duration,extras: extras);//rating noch rein
   }
+
+  List<dynamic> mediaItemListToSubsonicSongList(List<MediaItem> songList) {
+    List<dynamic> subsonicSongList = [];
+    for (MediaItem song in songList) {
+      subsonicSongList.add(mediaItemToSubsonicSong(song));
+    }
+    return subsonicSongList;
+  }
+
+  Map<dynamic,dynamic> mediaItemToSubsonicSong(MediaItem song) {
+    Map<dynamic,dynamic> subsonicSong = {
+      'id':song.id,
+      'title':song.title,
+      'album':song.album,
+      'artist':song.artist,
+      'duration':song.duration?.inSeconds,
+    };
+    song.extras?.forEach((key,value) {
+      subsonicSong[key] = value;
+    });
+    return subsonicSong;
+  }
 }
 
 class OtherScripts {
@@ -50,7 +72,6 @@ class OtherScripts {
 // Source - https://stackoverflow.com/a/73503029
 // Posted by eamirho3ein, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-09-08, License - CC BY-SA 4.0
-
 extension BoolOpposite on bool {
   bool opposite() {
     return this ? false : true;
